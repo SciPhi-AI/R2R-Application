d8d6ca223779bbd958bb354b1cbb0e09995241c0
NolanTrem <34580718+NolanTrem@users.noreply.github.com>
2024-07-01 15:44:40 -0700

    Update to use 1.0.28

--- src/pages/api/rag-completion.js
+++ src/pages/api/rag-completion.js
@@ -1,17 +1,10 @@
-import { r2rClient } from 'r2r-js';
-
-const DEFAULT_CONFIG = {
-  temperature: 0.1,
-  top_p: 1,
-  top_k: 100,
-  max_tokens_to_sample: 1024,
-  model: 'gpt-4-turbo',
-  search_limit: 10,
-};
+import { r2rClient, DEFAULT_GENERATION_CONFIG } from 'r2r-js';
 
 export const config = {
   runtime: 'edge',
 };
+
+const DEFAULT_SEARCH_LIMIT = 10;
 
 export default async function handler(req) {
   if (req.method !== 'POST') {
@@ -30,24 +23,27 @@ export default async function handler(req) {
     const searchFilters = {};
 
     const generationConfig = {
-      temperature: parseFloat(body.temperature) || DEFAULT_CONFIG.temperature,
-      top_p: parseFloat(body.topP) || DEFAULT_CONFIG.top_p,
-      top_k: parseInt(body.topK) || DEFAULT_CONFIG.top_k,
+      ...DEFAULT_GENERATION_CONFIG,
+      temperature:
+        parseFloat(body.temperature) ?? DEFAULT_GENERATION_CONFIG.temperature,
+      top_p: parseFloat(body.topP) ?? DEFAULT_GENERATION_CONFIG.top_p,
+      top_k: parseInt(body.topK) ?? DEFAULT_GENERATION_CONFIG.top_k,
       max_tokens_to_sample:
-        parseInt(body.maxTokensToSample) || DEFAULT_CONFIG.max_tokens_to_sample,
-      model: body.model || DEFAULT_CONFIG.model,
-      stream: true,
+        parseInt(body.maxTokensToSample) ??
+        DEFAULT_GENERATION_CONFIG.max_tokens_to_sample,
+      model: body.model || DEFAULT_GENERATION_CONFIG.model,
+      stream: body.stream ?? DEFAULT_GENERATION_CONFIG.stream,
     };
 
     const response = await client.rag(
       body.query,
       body.vectorSearch ?? true,
       searchFilters,
-      parseInt(body.searchLimit) || DEFAULT_CONFIG.search_limit,
+      parseInt(body.searchLimit) || DEFAULT_SEARCH_LIMIT,
       body.hybridSearch ?? false,
       body.useKnowledgeGraph ?? false,
-      generationConfig,
-      generationConfig
+      generationConfig, // for knowledge graph
+      generationConfig // for RAG
     );
 
     if (generationConfig.stream) {
