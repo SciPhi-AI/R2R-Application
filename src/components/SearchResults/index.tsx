7fc1dbdbbd844c1242c0ebe5d8c76ad63d867262
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2025-03-04 09:39:30 -0800

    Fix document id with Lilly (#201)
    * Fix document id with Lilly
    
    * Remove logging

--- src/components/SearchResults/index.tsx
+++ src/components/SearchResults/index.tsx
@@ -17,7 +17,7 @@ const VectorSearchResultItem: FC<{
   index: number;
   onOpenPdfPreview: (documentId: string, page?: number) => void;
 }> = ({ source, index, onOpenPdfPreview }) => {
-  const { documentId, metadata, text, score, extraction_id } = source;
+  const { document_id, metadata, text, score } = source;
 
   return (
     <div className="p-4 mb-2 flex items-center w-full">
@@ -33,11 +33,7 @@ const VectorSearchResultItem: FC<{
         </div>
 
         <p className="text-xs text-wrap break-words">{text}</p>
-        <p className="text-xs pt-4 text-zinc-500">
-          Document ID: {documentId}
-          <br />
-          Extraction ID: {extraction_id}
-        </p>
+        <p className="text-xs pt-4 text-zinc-500">Document ID: {document_id}</p>
       </div>
     </div>
   );
